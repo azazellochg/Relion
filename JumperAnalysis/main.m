222fdf63c40cf9c264882352a8c7b18b427261cc
Gregory Sharov <sharov.grigory@gmail.com>
2016-01-12 19:20:10 +0100

    Create main.m

---
+++ JumperAnalysis/main.m
@@ -0,0 +1,14 @@
+% main.m
+% last updated Oct 20, 2014
+% Bingxin Shen
+
+% To cite
+% B. Chen, B. Shen, and J. Frank
+% Particle migration analysis in iterative classification of cryo-EM single-particle data
+% J. Struct. Biology, 2014
+
+loadRELIONdata
+
+convergence
+
+jumpers
